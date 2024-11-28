650228755477a497065e3d06c4028ef2e07df50e
marxramos <marx.ramosquispe@gmail.com>
2024-11-28 12:34:25 -0500

    Arreglando el problema del botton dispersion

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ car_data = pd.read_csv('notebooks/vehicles_us.csv')
 
 # Crear un botón
 hist_button = st.button('Construir histograma')
+dist_button = st.button('construir una dispersion')
+
+#Encabezado
+st.header('Grafico de Automovil')
 
 # Al hacer clic en el botón
 if hist_button:
@@ -17,4 +21,10 @@ if hist_button:
     fig = px.histogram(car_data, x="odometer")
 
     # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if dist_button:
+    st.write('crecion de un grafico de dispersion para el conjunto de datos de anuncions de venta de coches')
+
+    fig = px.scatter(car_data, x ='model_year', y ='price')
     st.plotly_chart(fig, use_container_width=True)
